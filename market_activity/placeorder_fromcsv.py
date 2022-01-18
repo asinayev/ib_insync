1c89faf14ae9d1e52e5ce48137906f76a2953c34
asinayev <asinayev@gmail.com>
2022-01-18 20:39:10 +0000

    work on universal order entry

---
+++ market_activity/placeorder_fromcsv.py
@@ -0,0 +1,47 @@
+from ib_insync import *
+from connection import initiate
+from datetime import datetime, timedelta
+
+import argparse
+import csv
+
+# Instantiate the parser
+parser = argparse.ArgumentParser(description='limit buy stocks at listed limit prices on open and then sell at market close')
+
+parser.add_argument('--file', type=str, required=True)
+#File needs columns:
+# symbol
+# action
+# strike_price
+# order_type
+# time_in_force
+
+parser.add_argument('--real', dest='real', action = 'store_true') 
+parser.add_argument('--cash', type=int, required=True)
+args = parser.parse_args()
+
+ib = initiate.initiate_ib(args, 133) 
+
+stockdict = csv.DictReader(open(args.file, "r"))
+for row in stockdict:
+    if row['time_in_force']=='close':
+        if row['order_type'] == 'MKT':
+            ibkr_ordertype = 'MOC'
+        if row['order_type'] == 'LMT':
+            ibkr_ordertype = 'LOC'
+        else:
+            ibkr_ordertype = row['order_type']
+    else:
+        ibkr_ordertype = row['order_type']
+    row['contract']=Stock(row['symbol'], exchange='ISLAND', currency='USD')
+    row['quantity']=round(args.cash/float(row['strike_price']))
+    ib.qualifyContracts(row['contract'])
+    this_order = Order(action = row['action'],  
+                        orderType = ibkr_ordertype', 
+                        totalQuantity = row['quantity'], 
+                        tif = row['time_in_force'], 
+                        lmtPrice=row['strike_price'])
+    this_trade = ib.placeOrder(row['contract'], this_order)
+
+ib.disconnect()
+
