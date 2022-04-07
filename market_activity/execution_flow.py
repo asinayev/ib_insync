7a3eb77845d5f7c41a1ed6f2c96e1672d3133fac
asinayev <asinayev@gmail.com>
2022-04-07 16:42:52 -0400

    Clearer printing of fee events

--- market_activity/execution_flow.py
+++ market_activity/execution_flow.py
@@ -4,5 +4,9 @@ def fee_too_high(order_preset, contract, ib_conn, fee_limit):
     if type(test_trade)==list:
         return(False)
     else:
-        print(test_trade.maxCommission)
-        return test_trade.maxCommission>fee_limit
+        if test_trade.maxCommission>fee_limit:
+            print("PASS:", contract.symbol, "at", test_trade.maxCommission)
+            return True
+        else:
+            print("FAIL:", contract.symbol, "at", test_trade.maxCommission)
+            return False
