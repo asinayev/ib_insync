31300bb442cf69597ce33885ce3fe22fa70cdca4
asinayev <asinayev@gmail.com>
2022-08-15 17:22:26 -0400

    enable logging of close price with open price

--- market_activity/view_trades.py
+++ market_activity/view_trades.py
@@ -19,7 +19,7 @@ ib = initiate.initiate_ib(args, 14)
 order_csvs = {o:read_csv('/tmp/stonksanalysis/'+o+'.csv') for o in args.certain}
 order_stocks = {order:csv.symbol for order,csv in order_csvs.items()}
 
-trade_info = [[date.today().strftime("%m/%d/%Y"), 
+opens = [[date.today().strftime("%m/%d/%Y"), 
         t.contract.localSymbol, 
         t.order.action,
         str(t.fills[0].execution.price),
@@ -27,13 +27,20 @@ trade_info = [[date.today().strftime("%m/%d/%Y"),
         str(sum([f.execution.shares for f in t.fills])),
         str(t.orderStatus.status), 
         str(len(t.fills)), 
-        str(t.fills[0].execution.time)] for t in ib.trades() if len(t.fills)>0 ] 
-for t in trade_info:
-    if t[2]=='BUY':
-        t[2]=t[3]
-        t[3]=''
+        str(t.fills[0].execution.time)] for t in ib.trades() 
+        if len(t.fills)>0 and all([f.commissionReport.realizedPNL==0 for f in t.fills])] 
+closes = {t.contract.localSymbol:t.fills[0].execution.price for t in ib.trades()if (len(t.fills)>0 and any([f.commissionReport.realizedPNL!=0 for f in t.fills]))}
+
+for t in opens:
+    if t[1] in closes:
+        close_price = str(closes[t[1]])
     else:
-        t[2]=''
+        close_price = ""
+    if t[2]=='BUY':
+        t[2]=str(t[3])
+        t[3]=str(close_price)
+    else:
+        t[2]=close_price
 
     for order,stocks in order_stocks.items():
         if any(stocks==t[1]):
@@ -41,6 +48,11 @@ for t in trade_info:
             order_stocks[order]=order_stocks[order][stocks!=t[1]]
             break
     print(','.join(t))
+
+print("#############################")
+for sym in closes:
+    if sym not in (o[1] for o in opens):
+        print(sym,',',closes[sym])
 ib.disconnect()
 
 
