f39da291febdc97d0bc649f16f8bc909d66ab0c5
asinayev <asinayev@gmail.com>
2022-08-15 11:51:41 -0400

    improve logging functionality

--- market_activity/view_trades.py
+++ market_activity/view_trades.py
@@ -1,25 +1,44 @@
 from ib_insync import *
 from connection import initiate
 from datetime import date
+from glob import glob 
+from pandas import read_csv
 
 import argparse
 
 # Instantiate the parser
 parser = argparse.ArgumentParser(description='Cancel all open orders')
 
+
 parser.add_argument('--real', dest='real', action = 'store_true') 
+parser.add_argument('-c','--certain', action='append', help='Trades certainly executed', required=False)
 parser.set_defaults(feature=False)
 args = parser.parse_args()
 
 ib = initiate.initiate_ib(args, 14)
+order_csvs = {o:read_csv('/tmp/stonksanalysis/'+o+'.csv') for o in args.certain}
+order_stocks = {order:csv.symbol for order,csv in order_csvs.items()}
 
-x = [[date.today().strftime("%m/%d/%Y"), t.contract.localSymbol, t.order.action,str(t.fills[0].execution.price),str(t.fills[0].execution.shares),str(t.orderStatus.status), str(len(t.fills))] for t in ib.trades() if len(t.fills)>0 ] 
-for t in x:
+trade_info = [[date.today().strftime("%m/%d/%Y"), 
+        t.contract.localSymbol, 
+        t.order.action,
+        str(t.fills[0].execution.price),
+        '',
+        str(t.fills[0].execution.shares),
+        str(t.orderStatus.status), 
+        str(len(t.fills)), 
+        str(t.fills[0].execution.time)] for t in ib.trades() if len(t.fills)>0 ] 
+for t in trade_info:
     if t[2]=='BUY':
         t[2]=t[3]
         t[3]=''
     else:
         t[2]=''
+    for order,stocks in order_stocks.items():
+        if any(stocks==t[1]):
+            t[4]=order
+            order_stocks[order]=order_stocks[order][stocks!=t[1]]
+            break
     print(','.join(t))
 ib.disconnect()
 
