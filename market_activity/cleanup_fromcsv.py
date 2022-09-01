ae1541a9814ba02b520c73286ffcc6accca2f9ca
asinayev <asinayev@gmail.com>
2022-09-01 17:22:01 -0400

    Update order flow to use Adaptive algo

--- market_activity/cleanup_fromcsv.py
+++ market_activity/cleanup_fromcsv.py
@@ -33,49 +33,74 @@ openPositions = ib.positions()
 position_tickers = {p.contract.symbol:i for i,p in enumerate(openPositions)}
 open_tickers = [t.contract.symbol for t in ib.openTrades()]
 
+def order_conditions(args, position, lmt_price=None, contr):
+    time_condition = TimeCondition(isMore=True, time=datetime.today().strftime('%Y%m%d')+' 15:50:00 EST', conjunction='a')
+    price_condition = PriceCondition(1,conjunction='a', isMore=True,
+                        price=lmt_price, 
+                        exch='SMART', conId=contr.conId)
+    if args.illiquid and args.closetype=='last_high_eod' and position>0:
+        return Order(action="SELL",
+                     orderType="LMT",
+                     algoStrategy='Adaptive', 
+                     algoParams = [TagValue('adaptivePriority', 'Patient')],
+                     totalQuantity = abs(position), 
+                     tif = "DAY",
+                     conditions = [time_condition],
+                     lmtPrice=lmt_price)
+    elif args.closetype=='last_high_eod' and position<0:
+        return Order(action="BUY",
+                     orderType="MKT",
+                     algoStrategy='Adaptive', 
+                     algoParams = [TagValue('adaptivePriority', 'Patient')],
+                     totalQuantity = abs(position), 
+                     tif = "DAY",
+                     conditions = [time_condition,price_condition] )
+    elif args.illiquid and args.closetype=='mkt_eod' and position<0:
+        return Order(action="BUY",
+                     orderType="MKT",
+                     algoStrategy='Adaptive', 
+                     algoParams = [TagValue('adaptivePriority', 'Patient')],
+                     totalQuantity = abs(position), 
+                     tif = "DAY",
+                     conditions = [time_condition] )
+    elif args.illiquid and args.closetype=='mkt_eod' and position>0:
+        return Order(action="SELL",
+                     orderType="MKT",
+                     algoStrategy='Adaptive', 
+                     algoParams = [TagValue('adaptivePriority', 'Patient')],
+                     totalQuantity = abs(position), 
+                     tif = "DAY",
+                     conditions = [time_condition] )
+    elif not args.illiquid and args.closetype=='last_high_eod' and position>0:
+        return Order(action="SELL",
+                     orderType="LOC",
+                     totalQuantity = abs(position), 
+                     tif = None,
+                     lmtPrice=lmt_price)
+    elif not args.illiquid and args.closetype=='mkt_eod' and position<0:
+        return Order(action="BUY",
+                     orderType="MOC",
+                     totalQuantity = abs(position), 
+                     tif = None)
+    elif not args.illiquid and args.closetype=='mkt_eod' and position>0:
+        return Order(action="SELL",
+                     orderType="MOC",
+                     totalQuantity = abs(position), 
+                     tif = None)
+     
 for sym in stock_tickers:
     if sym in position_tickers and not sym in open_tickers:
         position = openPositions[position_tickers[sym]]
         contr = Stock(sym, exchange='SMART', currency='USD')
         ib.qualifyContracts(contr)
-        if args.illiquid or args.closetype=='last_high_eod':
-            actual_order_type='MIDPRICE'
-            tif='DAY'
-        else:
-            actual_order_type='MOC'
-            tif=None
-        if position.position>0:
-            ord_action="SELL"
-            if args.closetype=='last_high_eod' and not args.illiquid:
-                actual_order_type='LOC'
-                tif=None
-        else:
-            ord_action="BUY"
-        part_order = functools.partial(Order,
-                        action=ord_action,
-                        orderType=actual_order_type,
-                        totalQuantity = abs(position.position), 
-                        tif = tif,
-                        conditions = [])
-        if actual_order_type=='MIDPRICE':
-            if ord_action=="BUY":
-                lmt_price=round(float(position.avgCost*3),2)
-            else:
-                lmt_price=round(float(position.avgCost/3),2)
-            part_order=functools.partial(part_order, lmtPrice=lmt_price, conditions=[TimeCondition(isMore=True, time=datetime.today().strftime('%Y%m%d')+' 15:50:00 EST', conjunction='a')])
         if args.closetype=='last_high_eod':
             if sym not in current_moves:
                 print("Stock "+sym+" does not have current data. CLOSE MANUALLY")
                 continue 
             print("Closing stock "+sym+" at previous high")
-            order =part_order(lmtPrice = round(float(current_moves[sym]["high"]),2) )
-            if ord_action=='BUY':
-                order.conditions.append(
-                    PriceCondition(1,conjunction='a', isMore=True,
-                        price=round(float(current_moves[sym]["high"]),2), 
-                        exch='SMART', conId=contr.conId))
+            order=order_conditions(args, position=position.position, lmtPrice = round(float(current_moves[sym]["high"]),2), contr=contr) )
         else:
-            order = part_order() 
+            order=order_conditions(args, position=position.position, contr=contr) )
         ib.placeOrder(contr, order)
         print("ordering close for "+sym)
 
