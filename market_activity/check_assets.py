4918b24fad4c6e9cb3459a3955120ea5bb346cd1
asinayev <asinayev@gmail.com>
2023-03-26 12:14:23 -0400

    Rrecord close types and liquidities not asset types

--- market_activity/check_assets.py
+++ market_activity/check_assets.py
@@ -16,13 +16,15 @@ args = parser.parse_args()
 ib = initiate.initiate_ib(args, 133) 
 
 stockdict = csv.DictReader(open(args.file, "r"))
-asset_tickers={}
+close_types={}
+liquidities={}
 issues = []
 for row in stockdict:
-    if row['symbol'] in asset_tickers:
+    if row['symbol'] in close_types:
         issues.append("Repeated in asset file: "+ row['symbol'])
     else:
-        asset_tickers[row['symbol']]=row['asset_type']
+        close_types[row['symbol']]=row['close_type']
+        liquidities[row['symbol']]=row['liquid']
 
 openPositions = ib.positions()
 position_tickers = {p.contract.symbol:i for i,p in enumerate(openPositions)}
@@ -33,10 +35,11 @@ issues +=["Held but absent from asset file: "+ticker for ticker in position_tick
 if issues:
     print(*issues, sep = "\n")
     print("Correct asset file:\n")
-    correct_asset_file={sym:asset_tickers[sym] for sym in asset_tickers if sym in position_tickers}
+    correct_asset_file={sym:close_types[sym] for sym in close_types if sym in position_tickers}
     for ticker in position_tickers: 
-        if ticker not in asset_tickers:
-            correct_asset_file[ticker]='liquid'
-    print(*[t+','+correct_asset_file[t] for t in correct_asset_file],sep="\n")
+        if ticker not in close_types:
+            correct_asset_file[ticker]='UNKNOWN' #TODO: fill this with order type based on whether position is short
+            liquidities[ticker]='UNKNOWN' #TODO: fill this with liquidity based on volume
+    print(*[t+','+correct_asset_file[t]+','+liquidities[t] for t in correct_asset_file],sep="\n")
 
 ib.disconnect()
