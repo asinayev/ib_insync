3dd26e930582932da995c59b086cd7e2e138c014
asinayev <asinayev@gmail.com>
2023-11-14 23:06:39 -0500

    improve open trade order flow

--- market_activity/placeorder_fromcsv.py
+++ market_activity/placeorder_fromcsv.py
@@ -6,6 +6,7 @@ import transaction_logging
 import execution_flow
 import functools 
 import argparse
+import math
 import csv
 
 # Instantiate the parser
@@ -65,6 +66,18 @@ def get_quantity(row,existing_position, to_spend, price):
     else:
         return abs(existing_position), {'close':1}
 
+def get_price(ib,contract,is_limit, strike_price,action):
+    last_live=ib.reqMktData(contract, genericTickList='', snapshot=True, regulatorySnapshot=False,mktDataOptions=None)
+    while math.isnan(last_live.last):
+        ib.sleep(.1)
+    mkt_price=last_live.last
+    if is_limit:
+        return mkt_price, float(strike_price)
+    elif action=='BUY':
+        return mkt_price, mkt_price*1.1
+    elif action=='SELL':
+        return mkt_price, mkt_price*.9
+
 def get_position(ib,sym):
     openPositions = ib.positions()
     position_tickers = {p.contract.symbol:i for i,p in enumerate(openPositions)}
@@ -87,29 +100,25 @@ def place_order(row, ib):
     if row['time_in_force'] == 'close' and row['order_type'] in ['MKT', 'LMT']:
         ibkr_ordertype = {'MKT': 'MOC', 'LMT': 'LOC'}[row['order_type']]
     row['contract']=Stock(row['symbol'], exchange='SMART', currency='USD')
-    if ('strike_price' not in row or row['strike_price']=='') and 'close' not in row:
-        print("Stock has no strike price or close price. Skipping: "+row['symbol'])
-        return
-    if 'strike_price' not in row or row['strike_price']=='' :
-        row['strike_price']=float(row['close'])
-    else:
-        row['strike_price']=float(row['strike_price'])
+    ib.qualifyContracts(row['contract'])
+    if row['order_type']=='LMT':
+        if 'strike_price' not in row or row['strike_price']=='':
+            print("Stock has no strike price or close price. Skipping: "+row['symbol'])
+            return
+    row['strike_price'], lmt_price = get_price(ib,row['contract'],row['order_type']=='LMT',row['strike_price'],row['action'])
     current_position=get_position(ib,row['symbol'])
     row['quantity'], notes=get_quantity(row,current_position,args.cash,row['strike_price'])
-    ib.qualifyContracts(row['contract'])
-    lmt_price = float(row['strike_price'])
-    if row['order_type'] != 'LMT': lmt_price=lmt_price*1.1
     part_order = functools.partial(Order,
                         action = row['action'],
                         orderType = ibkr_ordertype, 
                         totalQuantity = row['quantity'], 
                         tif = row['time_in_force'], 
                         lmtPrice=round(lmt_price,2))
-    if row['strike_price']>args.minprice:
+    if row['strike_price']>args.minprice and row['strike_price']*row['quantity']<args.cash*1.5:
         print(f"Sending {ibkr_ordertype} order at {row['strike_price']}: {row['symbol']}")
         this_trade = ib.placeOrder(row['contract'], part_order())
         transaction_logging.log_trade(this_trade,args.file,'/tmp/stonksanalysis/order_logs.json',notes)
     else:
-        print(f"Skipping because price {row['strike_price']} is too low: {row['symbol']}")
+        print(f"Skipping because price {row['strike_price']} is too low or trying to order too much: {row['symbol']}")
 
 order_if_needed(args)
